88dabb92585112f4a5ece3b9ead7661224c811b2
aneesvr016 <aneesvr016@gmail.com>
2025-09-11 23:08:58 +0500

    setting

--- Assets/SequentialPlacement.cs
+++ Assets/SequentialPlacement.cs
@@ -13,16 +13,23 @@ public class SequentialPlacement : NetworkBehaviour {
     public NetworkGrabbable network;
 
     private int currentIndex = 0;             // Track current part
-    public bool check;
+    public bool check1,check2,check;
+    public string Tag,Tag1;
 
     private void OnTriggerEnter(Collider other) {
-        if(!Object.HasStateAuthority) return; // sirf server/host trigger handle kare
+       // if(!Object.HasStateAuthority) return; // sirf server/host trigger handle kare
 
         Collider partCollider = parts[currentIndex].GetComponentInChildren<Collider>();
 
-        if(partCollider != null && partCollider.bounds.Intersects(other.bounds)) {
+        if(other.tag==Tag) {
             Debug.Log($"[Server] Part {currentIndex} placed on table.");
             RPC_HandlePlacement();
+            transform.GetComponent<BoxCollider>().enabled = false;
+        }
+        else if (other.tag == Tag1 )
+        {
+            RPC_HandlePlacement();
+            transform.GetComponent<BoxCollider>().enabled = false;
         }
     }
 
@@ -30,32 +37,44 @@ public class SequentialPlacement : NetworkBehaviour {
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
     private void RPC_HandlePlacement() {
         // part disable
+        transform.GetComponent<BoxCollider>().enabled = false;
         parts[currentIndex].enabled = false;
 
         // snap to target position (agar available hai)
-        if(currentIndex < targetPoints.Count) {
-            Transform partTransform = parts[currentIndex].transform;
-            Transform target = targetPoints[currentIndex];
+       // if(currentIndex < targetPoints.Count) {
+           // Transform partTransform = parts[currentIndex].transform;
+         //   Transform target = targetPoints[currentIndex];
 
-            partTransform.position = target.position;
-            partTransform.rotation = target.rotation;
-        }
+       // parts[currentIndex].transform.position = targetPoints[currentIndex].transform.position;
+      //  parts[currentIndex].transform.rotation = targetPoints[currentIndex].transform.rotation;
+       // }
 
         // sequence logic
         if(parts.Count == 2) {
             currentIndex++;
             if(currentIndex == 1) {
+                //check1 = true;
                 parts[currentIndex].enabled = true;
             }
             else if(currentIndex == 2) {
                 network.enabled = true;
+                currentIndex = 0;
             }
         }
         else if(parts.Count == 1) {
             currentIndex++;
             if(currentIndex == 1 && !check) {
                 network.enabled = true;
+                currentIndex = 0;
             }
         }
+        Invoke("enable_Disable", 2f);
+    }
+
+
+
+    public void enable_Disable()
+    {
+        transform.GetComponent<BoxCollider>().enabled = true;
     }
 }
